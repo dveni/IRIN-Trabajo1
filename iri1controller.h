1076f8573db1d7fe453cfb425e7f84da3b91fd40
FONS <alfonso.mosanz@alumnos.upm.es>
2018-03-28 11:38:19 +0200

    el punto h

--- iri1controller.h
+++ iri1controller.h
@@ -1,7 +1,6 @@
 #ifndef IRI1CONTROLLER_H_
 #define IRI1CONTROLLER_H_
 
-
 /******************************************************************************/
 /******************************************************************************/
 
@@ -14,31 +13,39 @@ class CIri1Controller : public CController
 {
 public:
 
-    CIri1Controller (const char* pch_name, CEpuck* pc_epuck, int n_write_to_file);
+    CIri1Controller (const char* pch_name, CEpuck* pc_epuck, int n_wrtie_to_file);
     ~CIri1Controller();
     void SimulationStep(unsigned n_step_number, double f_time, double f_step_interval);
 
 private:
+		/* ROBOT */
     CEpuck* m_pcEpuck;
-    
+   
+	 	/* SENSORS */
 		CWheelsActuator* m_acWheels;
     CEpuckProximitySensor* m_seProx;
 		CRealLightSensor* m_seLight;
-		CRealBlueLightSensor* m_seBlueLight;
-		CRealRedLightSensor* m_seRedLight;
 		CContactSensor* m_seContact;
 		CGroundSensor* m_seGround;
 		CGroundMemorySensor* m_seGroundMemory;
-		CBatterySensor* m_seBattery;  
-		CBlueBatterySensor* m_seBlueBattery;  
-		CRedBatterySensor* m_seRedBattery;  
-		CEncoderSensor* m_seEncoder;  
-		CCompassSensor* m_seCompass;  
+		CBatterySensor* m_seBattery;   
+		CRealBlueLightSensor* m_seLightblue;
+		/* Global Variables */
+		double 		m_fLeftSpeed;
+		double 		m_fRightSpeed;
+		double**	m_fActivationTable;
+		int 			m_nWriteToFile;
+		double 		m_fTime;
+    double    fBattToForageInhibitor;
+		/* Functions */
 
-    float m_fOrientation; 
-    dVector2 m_vPosition;
+		void ExecuteBehaviors ( void );
+		void Coordinator ( void );
 
-		int m_nWriteToFile;
+		void ObstacleAvoidance ( unsigned int un_priority );
+		void Navigate ( unsigned int un_priority );
+		void GoLoad ( unsigned int un_priority );
+		void Forage ( unsigned int un_priority );
 };
 
 #endif
